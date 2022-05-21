5786fe57907afce4d2fb8f50a61b600ca568e441
Rafał Miczek <miczek.r@gmail.com>
2022-05-21 15:22:26 +0200

    Added initial training view.

---
+++ lib/modules/main/training/training.controller.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:projekt_pum/modules/main/training/training.dart';
+import 'package:projekt_pum/modules/main/training/training.view.dart';
+
+class TrainingPageController extends State<TrainingPage> {
+  TrainingPageController();
+
+  @override
+  Widget build(BuildContext context) => TrainingPageView(this);
+}
