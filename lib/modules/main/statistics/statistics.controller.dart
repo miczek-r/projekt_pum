bd0a37c49e327e85a542c2a9d6e25372aa713683
Rafał Miczek <miczek.r@gmail.com>
2022-05-21 15:22:17 +0200

    Added initial statistics view.

---
+++ lib/modules/main/statistics/statistics.controller.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:projekt_pum/modules/main/statistics/statistics.dart';
+import 'package:projekt_pum/modules/main/statistics/statistics.view.dart';
+
+class StatisticsPageController extends State<StatisticsPage> {
+  StatisticsPageController();
+
+  @override
+  Widget build(BuildContext context) => StatisticsPageView(this);
+}
---
+++ lib/modules/main/statistics/statistics.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:projekt_pum/modules/main/statistics/statistics.controller.dart';
+
+class StatisticsPage extends StatefulWidget {
+  @override
+  StatisticsPageController createState() => StatisticsPageController();
+}
