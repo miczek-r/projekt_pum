4004ef17a944e2db6d6496ba05b9e7ac1cba015e
Rafał Miczek <miczek.r@gmail.com>
2022-06-14 01:06:42 +0200

    Added equations game

---
+++ lib/modules/games/math/equations/main/equations.main.view.dart
@@ -0,0 +1,32 @@
+import 'package:animations/animations.dart';
+import 'package:flutter/material.dart';
+import 'package:projekt_pum/modules/games/reaction/sound_reaction/main/sound_reaction.main.controller.dart';
+import 'package:projekt_pum/modules/games/reaction/sound_reaction/main/sound_reaction.main.dart';
+import 'package:widget_view/widget_view.dart';
+
+import 'equations.main.controller.dart';
+import 'equations.main.dart';
+
+class EquationsMainPageView extends StatefulWidgetView<
+    EquationsMainPage, EquationsMainPageController> {
+  const EquationsMainPageView(EquationsMainPageController controller,
+      {Key? key})
+      : super(controller, key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageTransitionSwitcher(
+          duration: const Duration(milliseconds: 500),
+          transitionBuilder: (Widget child, Animation<double> animation,
+              Animation<double> secondaryAnimation) {
+            return SharedAxisTransition(
+                child: child,
+                animation: animation,
+                secondaryAnimation: secondaryAnimation,
+                transitionType: SharedAxisTransitionType.horizontal);
+          },
+          child: controller.widgetOptions.elementAt(controller.selectedIndex)),
+    );
+  }
+}
